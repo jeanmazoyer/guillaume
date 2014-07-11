8720445937b6f907c1bceaaa630a6e3b5ca4f351
Jean Mazoyer <jeanmazoyer@gmail.com>
2014-07-11 20:13:13 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guillaume::Application.config.secret_token = '2d87318eb559f976c6b0dd8f5fc965d3f082b05c74f12cbd1bd0be1da3cd36c9cb4c02332360a19ac036295518f40123bc7d6f493136015b30c7b432a97992b2'
